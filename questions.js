8585c6f6ed4263a64b3044fb86273a8693d7d795
Jared Simpson <66752595+simpson-482@users.noreply.github.com>
2022-06-06 22:19:46 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,114 @@
+// Creating an array and passing the number, questions, options, and answers
+
+let questions = [
+    {
+        numb: 1,
+        question: "Which of the following was a Cleveland Browns runningback?",
+        answer: "Jim Brown",
+        options: [
+            "Walter Payton",
+            "Jim Brown",
+            "Larry Csonka",
+            "Herschel Walker"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which country has won the most World Cups?",
+        answer: "Brazil",
+        options: [
+            "Argentina",
+            "Germany",
+            "France",
+            "Brazil"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Who was the 32nd president of the United States?",
+        answer: "Franklin Roosevelt",
+        options: [
+            "Franklin Roosevelt",
+            "Ronald Reagan",
+            "Theodore Rooevelt",
+            "Thomas Jefferson"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which of the following is the eleventh element in the periodic table?",
+        answer: "Sodium",
+        options: [
+            "Aluminum",
+            "Carbon",
+            "Sodium",
+            "Sulfur"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which of the following is an amino acid?",
+        answer: "Glutamine",
+        options: [
+            "Glutamine",
+            "Guanine",
+            "Purine",
+            "Neuron"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which is the volcanoe that produces lava in Hawaii Valcanoes National Park?",
+        answer: "Kilauea",
+        options: [
+            "Kilauea",
+            "Mount Vesuvius",
+            "Yellowstone",
+            "Mount Pelee"
+        ]
+    },
+    {
+        numb: 7,
+        question: "What is the largest lake in the world?",
+        answer: "Lake Superior",
+        options: [
+            "Lake Michigan",
+            "Lake Superior",
+            "Caspian Sea",
+            "Lake Baikal"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Which of the following is a Dow Jones Company?",
+        answer: "Apple",
+        options: [
+            "American Airlines",
+            "Apple",
+            "Facebook",
+            "Netflix"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What is the component that provides the basis of life for coral reefs?",
+        answer: "Calcium Carbonate",
+        options: [
+            "Calcium Carbonate",
+            "Nitrogen and Phosphorus",
+            "Salt Water and Cold Water",
+            "Fish and Algae"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Who is the Greek God of the sun?",
+        answer: "Apollo",
+        options: [
+            "Zeus",
+            "Aphrodite",
+            "Hermes",
+            "Apollo"
+        ]
+    },
+]
